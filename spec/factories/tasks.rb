96ffa53fb5dd2177a1aac15a385ca78750ae9601
ryusuke.sato <jftrx029@gmail.com>
2021-01-02 22:44:37 +0900

    Add:factorybot追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,16 @@
+# t.string "title"
+# t.text "content"
+# t.integer "status"
+# t.datetime "deadline"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+# t.integer "user_id"
+# t.index ["user_id"], name: "index_tasks_on_user_id"
+
 FactoryBot.define do
   factory :task do
-    
+    title { "title" }
+    status { 0 }
+    association :user
   end
 end
