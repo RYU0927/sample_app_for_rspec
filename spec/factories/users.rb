f4229c28a2b8a62d9cadc8e3e9bbbd0802cfa331
ryusuke.sato <jftrx029@gmail.com>
2021-01-02 23:39:48 +0900

    Fix:解答確認後　emailをexample.comに変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,8 +7,8 @@
 
 FactoryBot.define do
   factory :user do
-    email { "hoge@foo.bar" }
+    email { "hoge@example.com" }
     password { "password" }
-    password_confirmation { "password" }
+    password_confirmation { "password"}
   end
 end
