76fbe835db264df52b6b1670d9915d9ce7842e02
ryusuke.sato <jftrx029@gmail.com>
2021-01-03 13:10:19 +0900

    Fix:after confirming the answer. Added test for error messages

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Task, type: :model do
       it 'is valid with all attributes' do
         task = build(:task)
         expect(task).to be_valid
+        expect(task.errors[:title]).to_not be_empty
       end
       it 'is invalid without title' do
         task = build(:task, title: nil)
