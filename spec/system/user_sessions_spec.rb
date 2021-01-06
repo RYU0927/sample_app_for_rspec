4231cf4ed91bd1c6ba1be6a062c4bebcb20a3ccc
ryusuke.sato <jftrx029@gmail.com>
2021-01-06 12:46:36 +0900

    Add:add user_sessions contents

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -3,16 +3,31 @@ require 'rails_helper'
 RSpec.describe 'UserSessions', type: :system do
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
-      it 'ログイン処理が成功する'
+      it 'ログイン処理が成功する' do
+        user = create(:user)
+        login(user)
+      end
     end
     context 'フォームが未入力' do
-      it 'ログイン処理が失敗する'
+      it 'ログイン処理が失敗する' do
+        user = create(:user)
+        visit login_path
+        fill_in 'email', with: nil
+        fill_in 'password', with: nil
+        click_button "Login"
+        expect(page).to have_content("Login failed")
+      end
     end
   end
 
   describe 'ログイン後' do
     context 'ログアウトボタンをクリック' do
-      it 'ログアウト処理が成功する'
+      it 'ログアウト処理が成功する' do
+        user = create(:user)
+        login(user)
+        click_link "Logout"
+        expect(page).to have_content("Logged out")
+      end
     end
   end
 end
