1e9327b721e8b6060b0131857838bba1172d2816
ryusuke.sato <jftrx029@gmail.com>
2021-01-06 20:40:46 +0900

    Fix:users_sessions_spec code refactoring with let

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'UserSessions', type: :system do
+  let(:user){create(:user)}
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する' do
-        user = create(:user)
+
         login(user)
       end
     end
     context 'フォームが未入力' do
       it 'ログイン処理が失敗する' do
-        user = create(:user)
+
         visit login_path
         fill_in 'email', with: nil
         fill_in 'password', with: nil
@@ -23,7 +24,7 @@ RSpec.describe 'UserSessions', type: :system do
   describe 'ログイン後' do
     context 'ログアウトボタンをクリック' do
       it 'ログアウト処理が成功する' do
-        user = create(:user)
+
         login(user)
         click_link "Logout"
         expect(page).to have_content("Logged out")
